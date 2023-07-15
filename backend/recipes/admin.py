d36dddbeb9be5aba2eb0fa47f5186ec7484f2147
Евгений Горин <evg_gorin@mail.ru>
2023-07-15 14:34:55 +0400

    Ревью.Fix bugs...

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -26,8 +26,8 @@ class IngredientAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    fields = ('author', 'name', 'text', 'cooking_time', 'likes')
-    list_display = ('id', 'author', 'name', 'cooking_time', 'likes')
+    fields = ('author', 'name', 'likes')
+    list_display = ('id', 'author', 'name', 'likes')
     list_filter = ('author', 'name', 'tags')
     inlines = (IngredientInline,)
 
